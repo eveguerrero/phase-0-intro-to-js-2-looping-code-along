988c2222795692a63788ce3d7280619e686336c7
Eveline Guerrero <evelineguerrero23@gmail.com>
2021-11-13 23:54:15 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+let namesArray = ["Ada", "Brendan", "Ali"];
+let event = birthday;
+
+  
+
+  function writeCards(namesArray, event){
+      let result = [];
+    for(let x = 0; x < namesArray.length; x++){
+        result.push(`Thank you, ${namesArray[x]}, for the wonderful ${event} gift!`)
+        x++;
+       
+    }
+    return result;
+
+  }
+
+  function countDown(num){
+    while(num >= 0){
+       
+    console.log(num);
+    num--;
+    }
+    
+      }
